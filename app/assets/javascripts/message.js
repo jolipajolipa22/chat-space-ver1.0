034318af31c077f9342ebeb8cb684a4ea54c6275
jolipajolipa22 <jolipajolipa22@gmail.com>
2019-07-12 19:01:44 +0900

    create-update-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,62 +1,88 @@
-$(function(){
-function buildHTML(message){
-  // if ( message.image ) {
-  //   var image = `<asset_path src=${message.image} >`
-  //    return html;
-  // } else {
-  //   var image = ``
-     
-  //    return html;
-  // };
+$(function () {
+    function buildHTML(message) {
 
-  var image = message.image ? `<asset_path src=${message.image} >` : ``;
+    var img ="";
+        if (message.image){
+        var img = `<img src = ${message.image} class: "lower-message_image">`
+        }
+        var html = `<div class=" message data-message-id="${message.id}">
+                      <div class= "message__box__username">
+                        <h4>${message.user_name}</h4>
+                      </div>
+                      <div class= "message__box__time">
+                        <p>${message.created_at}</p>
+                      </div>
+                      　 <div class= "message__box__content">
+                      <p>${message.content}</p>
+                          ${img}
+                      </div>
+                    </div>`
+        return html;
+  }
   
-  var html = `<div class="message" data-message-id=${message.id}>
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-
-                      <div class="upper-message__date">
-                        ${message.date}
-                      </div>
-                    </div>
-
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                        ${message.content}
-                      </p>
-                    </div>
-                  ${image}
-                </div>`
-
-　 return html; 
-}
 
 
-
-$('.js-form').on('submit', function(e){
-e.preventDefault();
+    $('#new_message').on('submit', function (e) {
+    e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(data){
+    console.log('event OK')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+      .done(function (data) {
+        console.log(data)
         var html = buildHTML(data);
-        $('.messages').append(html);
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
-        $('form')[0].reset();
+        // $('.messages').append(html);
+        // $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast'); 
+        // $('form')[0].reset();
+      $('.messages').append(html)
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight},'fasts')
+      $('.submit').prop("disabled", false)
+      $('#message_content').val("");
+      $('#message_image').val("");
       })
-        .fail(function(){
-          alert('error');
-        })
-        return false;
-        })
-});
+      .fail(function () {
+        alert('error');
+      })
+      .always(() => {
+        $(".form__submit").removeAttr("disabled");
+        });
+    // return false;
+    // })
+  });
 
+  //自動更新
+  var reloadMessages = function () {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+    var last_message_id = $('.message:last').data("message-id");
+    console.log('aaaaaa');
+
+    $.ajax({
+      url: "api/messages",
+      type: "GET",
+      dataType: "json",
+      data: { last_id: last_message_id }
+    })
+      .done(function (messages) {
+        var insertHTML = '';//追加するHTMLの入れ物を作る
+        messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+          insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
+          $('.messages').append(insertHTML);//メッセージを追加
+        })
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
+      })
+      .fail(function () {
+        alert('自動更新に失敗しました');//ダメだったらアラートを出す
+      });
+      
+    }
+   };
+    setInterval(reloadMessages, 5000);//5000ミリ秒ごとにreloadMessagesという関数を実行し自動更新を行う。
+
+});
