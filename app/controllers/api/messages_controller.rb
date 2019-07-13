43da320ce303b684a3fde5eade1cc8f43acb0c47
jolipajolipa22 <jolipajolipa22@gmail.com>
2019-07-13 14:24:12 +0900

    create-update-function6

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
-    # @messages = Messages.includes(:user).where('id > ?', params[:group_id])
-    @group = Group.find(params[:group_id]) 
-    @messages = @group.messages.includes(:user).where('id > ?', params[:last_id]) 
+    @messages = Messages.includes(:user).where('id > ?', params[:group_id])
   end
 end
 
